9967322fe024f905dadcc655f66100541adc153a
Allen Zhang <ztao165@163.com>
2023-05-08 20:29:15 -0700

    Initial commit from create-wagmi

---
+++ src/wagmi.ts
@@ -0,0 +1,23 @@
+import { w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { configureChains, createClient } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+
+export const walletConnectProjectId = 'dba7331053371470365be9206718fb4d'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [w3mProvider({ projectId: walletConnectProjectId })],
+)
+
+export const client = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({
+    chains,
+    projectId: walletConnectProjectId,
+    version: 1,
+  }),
+  provider,
+  webSocketProvider,
+})
+
+export { chains }
